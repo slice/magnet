a56e0695bcde6d17ba64b447c6df69626008b176
slice <ryaneft@gmail.com>
2020-07-01 13:07:44 -0700

    Update to iOS 14

---
+++ Magnet/Magnet.swift
@@ -0,0 +1,15 @@
+//
+//  App.swift
+//  Magnet
+//
+
+import SwiftUI
+
+@main
+struct Magnet: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
