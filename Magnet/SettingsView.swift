d6c1404924bb91b473e00e13e723277bda147535
slice <ryaneft@gmail.com>
2020-07-02 12:01:01 -0700

    Improve SettingsView
    Turn timerArmDelay into a slider, and add a bunch of footers everywhere.
    That should do it.

--- Magnet/SettingsView.swift
+++ Magnet/SettingsView.swift
@@ -8,17 +8,44 @@ import SwiftUI
 struct SettingsView: View {
     @EnvironmentObject var settings: SettingsStore
 
+    var timerArmDelayBinding: Binding<Double> {
+        Binding {
+            Double(settings.timerArmDelay)
+        } set: { newValue in
+            settings.timerArmDelay = Int(newValue)
+        }
+    }
+
+    var timerArmDelayDescription: String {
+        "\(Double(settings.timerArmDelay) / 1000)s"
+    }
+
     var body: some View {
         Form {
-            Section(header: Text("Timer")) {
-                Stepper(value: $settings.timerArmDelay, in: 0...1000, step: 25) {
-                    Text("Safety: \(settings.timerArmDelay)ms")
+            Section(
+                header: Text("Timer Safety"),
+                footer: Text("Adjust the amount of time you have to hold your tap before the timer can start.")
+            ) {
+                Slider(
+                    value: timerArmDelayBinding,
+                    in: 25...1000,
+                    step: 25,
+                    minimumValueLabel: Image(systemName: "hare"),
+                    maximumValueLabel: Image(systemName: "tortoise")
+                ) {
+                    Text(timerArmDelayDescription)
+                }
+                Text(timerArmDelayDescription).foregroundColor(.secondary)
+            }
+            Section(
+                header: Text("Timer Display"),
+                footer: Text("Monospaced digits will always be used, even when a monospaced font is not in use.")
+            ) {
+                Toggle(isOn: $settings.timerHideTimeWhileActive) {
+                    Text("Hide Time While Running")
                 }
                 Toggle(isOn: $settings.timerMonospaceFont) {
                     Text("Monospaced Font")
-                }
-                Toggle(isOn: $settings.timerHideTimeWhileActive) {
-                    Text("Hide Time While Running")
                 }
             }
         }
@@ -27,8 +54,6 @@ struct SettingsView: View {
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        let store = SettingsStore()
-
-        return SettingsView().environmentObject(store)
+        return SettingsView().environmentObject(SettingsStore())
     }
 }
